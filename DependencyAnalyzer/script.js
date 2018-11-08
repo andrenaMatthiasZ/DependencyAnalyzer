29a65579501a6a53c0732e7d339c880bf8d63f2d
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-11-08 11:15:38 +0100

    add first text to table

---
+++ DependencyAnalyzer/script.js
@@ -0,0 +1,16 @@
+﻿
+
+const createTable = function(n) {
+
+    const table = document.getElementById("dependencyTable");
+
+
+    table.insertRow();
+    const firstRow = table.insertRow();
+    table.insertRow();
+
+    const firstCell = firstRow.insertCell();
+    firstCell.innerHTML = "first cell";
+};
+
+createTable(2);
