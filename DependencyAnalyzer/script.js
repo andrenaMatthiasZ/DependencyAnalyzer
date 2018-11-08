4bb5d763377a3c302af80dbee4375a164e8de2e4
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-11-08 13:32:04 +0100

    create multiple cells

--- DependencyAnalyzer/script.js
+++ DependencyAnalyzer/script.js
@@ -5,12 +5,24 @@ const createTable = function(n) {
     const table = document.getElementById("dependencyTable");
 
 
-    table.insertRow();
-    const firstRow = table.insertRow();
-    table.insertRow();
+    const rowIndices = [...Array(n)].map((_, index) => index);
+    rowIndices.forEach(i => createRow( table,i, n) );
 
-    const firstCell = firstRow.insertCell();
-    firstCell.innerHTML = "first cell";
+};
+
+const createRow = function(table,i, n) {
+    const row = table.insertRow();
+
+    const firstCell = row.insertCell();
+    const columnIndices = [...Array(n)].map((_,index)=>index);
+    columnIndices.forEach(j => createCell(row,i,j));
+    firstCell.innerHTML = "";
+};
+
+const createCell = function (row, i, columnIndex) {
+
+    const cell = row.insertCell();
+    cell.innerHTML = columnIndex + i;
 };
 
 createTable(2);
